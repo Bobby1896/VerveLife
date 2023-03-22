3f79604016e913bf7a933c95f37612e6cc9285e9
jonathan.alabi <alabijonathan600@gmail.com>
2023-03-22 23:20:09 +0100

    Keyboard Aware Scroll View

--- components/AccountCreation.js
+++ components/AccountCreation.js
@@ -11,6 +11,7 @@ import {
 import React, {useState} from 'react';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import {Button} from 'react-native/Libraries/Components/Button';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 // import CheckBox from '@react-native-community/checkbox';
 
 const AccountCreation = ({navigation}) => {
@@ -18,142 +19,143 @@ const AccountCreation = ({navigation}) => {
   const [submit, setSubmit] = useState(false);
   return (
     <SafeAreaView>
-      <ScrollView>
-        <View
-          style={{
-            marginTop: 70,
-            borderBottomWidth: 3,
-            borderBottomColor: '#E8E8E8',
-            paddingBottom: 20,
-            flexDirection: 'row',
-            justifyContent: 'center',
-          }}>
-          <FontAwesome5
-            onPress={() => navigation.navigate('Login')}
-            style={{color: '#00425F', fontSize: 28, marginRight: 110}}
-            name={'chevron-left'}
-          />
-          <Text
+      <KeyboardAwareScrollView>
+        <ScrollView>
+          <View
             style={{
-              fontSize: 18,
-              fontWeight: 400,
-              textAlign: 'center',
-              marginRight: 100,
-              color: '#4B4B4B',
+              marginTop: 70,
+              borderBottomWidth: 3,
+              borderBottomColor: '#E8E8E8',
+              paddingBottom: 20,
+              flexDirection: 'row',
+              justifyContent: 'center',
             }}>
-            Create an Account
-          </Text>
-        </View>
-
-        <Text
-          style={{
-            marginLeft: 15,
-            marginTop: 20,
-            color: '#00425F',
-            fontSize: 20,
-            fontWeight: 600,
-          }}>
-          Tell us about you.
-        </Text>
-
-        <View style={{alignItems: 'center', marginTop: 10}}>
-          <View style={{flexDirection: 'row', gap: 10, marginBottom: 10}}>
-            <TextInput
-              placeholder="First Name"
-              style={{
-                backgroundColor: '#E8E8E8',
-                height: 70,
-                paddingLeft: 15,
-                width: 180,
-              }}
+            <FontAwesome5
+              onPress={() => navigation.navigate('Login')}
+              style={{color: '#00425F', fontSize: 28, marginRight: 110}}
+              name={'chevron-left'}
             />
-            <TextInput
-              placeholder="Last Name"
+            <Text
               style={{
-                backgroundColor: '#E8E8E8',
-                height: 70,
-                paddingLeft: 15,
-                width: 180,
-              }}
-            />
+                fontSize: 18,
+                fontWeight: 400,
+                textAlign: 'center',
+                marginRight: 100,
+                color: '#4B4B4B',
+              }}>
+              Create an Account
+            </Text>
           </View>
 
-          <View>
-            <TextInput
-              placeholder="Email Address"
-              style={{
-                backgroundColor: '#E8E8E8',
-                width: 370,
-                paddingLeft: 15,
-                marginBottom: 10,
-                height: 70,
-              }}
-            />
-            <TextInput
-              placeholder="Phone Number"
-              keyboardType="numeric"
-              style={{
-                backgroundColor: '#E8E8E8',
-                width: 370,
-                paddingLeft: 15,
-                marginBottom: 10,
-                height: 70,
-              }}
-            />
-            <TextInput
-              placeholder="Password"
-              secureTextEntry={true}
-              style={{
-                backgroundColor: '#E8E8E8',
-                width: 370,
-                paddingLeft: 15,
-                marginBottom: 10,
-                height: 70,
-              }}
-            />
-            <TextInput
-              placeholder="Confirm Password"
-              secureTextEntry={true}
-              style={{
-                backgroundColor: '#E8E8E8',
-                width: 370,
-                paddingLeft: 15,
-                marginBottom: 10,
-                height: 70,
-              }}
-            />
-            <TextInput
-              placeholder="Gender"
-              style={{
-                backgroundColor: '#E8E8E8',
-                width: 370,
-                paddingLeft: 15,
-                marginBottom: 10,
-                height: 70,
-              }}
-            />
-            <TextInput
-              placeholder="Country"
-              style={{
-                backgroundColor: '#E8E8E8',
-                width: 370,
-                paddingLeft: 15,
-                marginBottom: 10,
-                height: 70,
-              }}
-            />
-            <TextInput
-              placeholder="Referral Code (Optional)"
-              style={{
-                backgroundColor: '#E8E8E8',
-                width: 370,
-                paddingLeft: 15,
-                marginBottom: 10,
-                height: 70,
-              }}
-            />
+          <Text
+            style={{
+              marginLeft: 15,
+              marginTop: 20,
+              color: '#00425F',
+              fontSize: 20,
+              fontWeight: 600,
+            }}>
+            Tell us about you.
+          </Text>
 
-            {/* <View>
+          <View style={{alignItems: 'center', marginTop: 10}}>
+            <View style={{flexDirection: 'row', gap: 10, marginBottom: 10}}>
+              <TextInput
+                placeholder="First Name"
+                style={{
+                  backgroundColor: '#E8E8E8',
+                  height: 70,
+                  paddingLeft: 15,
+                  width: 180,
+                }}
+              />
+              <TextInput
+                placeholder="Last Name"
+                style={{
+                  backgroundColor: '#E8E8E8',
+                  height: 70,
+                  paddingLeft: 15,
+                  width: 180,
+                }}
+              />
+            </View>
+
+            <View>
+              <TextInput
+                placeholder="Email Address"
+                style={{
+                  backgroundColor: '#E8E8E8',
+                  width: 370,
+                  paddingLeft: 15,
+                  marginBottom: 10,
+                  height: 70,
+                }}
+              />
+              <TextInput
+                placeholder="Phone Number"
+                keyboardType="numeric"
+                style={{
+                  backgroundColor: '#E8E8E8',
+                  width: 370,
+                  paddingLeft: 15,
+                  marginBottom: 10,
+                  height: 70,
+                }}
+              />
+              <TextInput
+                placeholder="Password"
+                secureTextEntry={true}
+                style={{
+                  backgroundColor: '#E8E8E8',
+                  width: 370,
+                  paddingLeft: 15,
+                  marginBottom: 10,
+                  height: 70,
+                }}
+              />
+              <TextInput
+                placeholder="Confirm Password"
+                secureTextEntry={true}
+                style={{
+                  backgroundColor: '#E8E8E8',
+                  width: 370,
+                  paddingLeft: 15,
+                  marginBottom: 10,
+                  height: 70,
+                }}
+              />
+              <TextInput
+                placeholder="Gender"
+                style={{
+                  backgroundColor: '#E8E8E8',
+                  width: 370,
+                  paddingLeft: 15,
+                  marginBottom: 10,
+                  height: 70,
+                }}
+              />
+              <TextInput
+                placeholder="Country"
+                style={{
+                  backgroundColor: '#E8E8E8',
+                  width: 370,
+                  paddingLeft: 15,
+                  marginBottom: 10,
+                  height: 70,
+                }}
+              />
+              <TextInput
+                placeholder="Referral Code (Optional)"
+                style={{
+                  backgroundColor: '#E8E8E8',
+                  width: 370,
+                  paddingLeft: 15,
+                  marginBottom: 10,
+                  height: 70,
+                }}
+              />
+
+              {/* <View>
               <View style={styles.checkboxContainer}>
                 <CheckBox
                   value={isSelected}
@@ -172,74 +174,75 @@ const AccountCreation = ({navigation}) => {
                 <Text>I agree I am 13 years old or above</Text>
               </View>
             </View> */}
-          </View>
-
-          <View>
-            <Text
-              onPress={() => {
-                setSubmit(true);
-              }}
-              style={{
-                backgroundColor: '#3077BD',
-                width: 360,
-                textAlign: 'center',
-                color: '#FFFFFF',
-                padding: 15,
-                fontSize: 16,
-                marginBottom: 20,
-                fontWeight: 600,
-                borderRadius: 10,
-              }}>
-              CREATE ACCOUNT
-            </Text>
-          </View>
-
-          <Modal transparent={true} visible={submit}>
-            <View style={{backgroundColor: '#000000aa', flex: 1}}>
-              <View
-                style={{
-                  backgroundColor: '#FFFFFF',
-                  margin: 50,
-                  flex: 0.5,
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  borderRadius: 20,
-                  marginTop: 210,
-                }}>
-                <Text
-                  style={{
-                    fontSize: 28,
-                    textAlign: 'center',
-                    marginBottom: 20,
-                    fontWeight: 600,
-                  }}>
-                  Account Created Successfully
-                  <FontAwesome5
-                    style={{color: 'green', fontSize: 25}}
-                    name={'check-circle'}
-                  />
-                </Text>
-
-                <Text
-                  onPress={() => navigation.navigate('Registration Info')}
-                  style={{
-                    backgroundColor: '#3077BD',
-                    width: 100,
-                    textAlign: 'center',
-                    color: '#FFFFFF',
-                    padding: 15,
-                    fontSize: 16,
-                    marginBottom: 20,
-                    fontWeight: 600,
-                    borderRadius: 20,
-                  }}>
-                  Done
-                </Text>
-              </View>
             </View>
-          </Modal>
-        </View>
-      </ScrollView>
+
+            <View>
+              <Text
+                onPress={() => {
+                  setSubmit(true);
+                }}
+                style={{
+                  backgroundColor: '#3077BD',
+                  width: 360,
+                  textAlign: 'center',
+                  color: '#FFFFFF',
+                  padding: 15,
+                  fontSize: 16,
+                  marginBottom: 20,
+                  fontWeight: 600,
+                  borderRadius: 10,
+                }}>
+                CREATE ACCOUNT
+              </Text>
+            </View>
+
+            <Modal transparent={true} visible={submit}>
+              <View style={{backgroundColor: '#000000aa', flex: 1}}>
+                <View
+                  style={{
+                    backgroundColor: '#FFFFFF',
+                    margin: 50,
+                    flex: 0.5,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    borderRadius: 20,
+                    marginTop: 210,
+                  }}>
+                  <Text
+                    style={{
+                      fontSize: 28,
+                      textAlign: 'center',
+                      marginBottom: 20,
+                      fontWeight: 600,
+                    }}>
+                    Account Created Successfully
+                    <FontAwesome5
+                      style={{color: 'green', fontSize: 25}}
+                      name={'check-circle'}
+                    />
+                  </Text>
+
+                  <Text
+                    onPress={() => navigation.navigate('Registration Info')}
+                    style={{
+                      backgroundColor: '#3077BD',
+                      width: 100,
+                      textAlign: 'center',
+                      color: '#FFFFFF',
+                      padding: 15,
+                      fontSize: 16,
+                      marginBottom: 20,
+                      fontWeight: 600,
+                      borderRadius: 20,
+                    }}>
+                    Done
+                  </Text>
+                </View>
+              </View>
+            </Modal>
+          </View>
+        </ScrollView>
+      </KeyboardAwareScrollView>
     </SafeAreaView>
   );
 };
