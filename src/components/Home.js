e64a2544b3cea09fe27523926387737696772c91
jonathan.alabi <alabijonathan600@gmail.com>
2023-04-12 22:58:21 +0100

    Environment SetUp

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="grid grid-cols-3 ">
+      <div className="px-9 text-lg text-red-300 font-bold"> Winamps</div>
+
+      <div>
+        <h2 className="px-9 text-lg text-red-300 font-bold">Music</h2>
+      </div>
+
+      <div>
+        <h2>App</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
