3ce706d797204735094722aa467fe89c05b99581
jonathan.alabi <alabijonathan600@gmail.com>
2023-05-11 10:57:38 +0100

    Responsiveness / footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "./components/Home";
+import SideBar from "./components/SideBar";
 
 function App() {
   return (
     <div>
       <Home />
+      {/* <SideBar /> */}
     </div>
   );
 }
